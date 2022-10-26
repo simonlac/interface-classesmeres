30e5a5a067efaba6ac0875500131f52f46f668e4
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:40:27 -0400

    Add files via upload

---
+++ Interface_ClassesMeres_/Interface_ClassesMeres/Interface_ClassesMeres/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_ClassesMeres
+{
+    class Program
+    {
+        interface IVolant
+        {
+            void Decoller();
+            void Atterrir();
+        }
+        
+        class Oiseau : IVolant
+        {
+            public void Decoller()
+            {
+                Console.WriteLine("Je décolle de mon nid...\n");
+            }
+            public void Atterrir()
+            {
+                Console.WriteLine("J'atterris dans mon nid...\n");
+            }
+        }
+        class Avion : IVolant
+        {
+            public void Decoller()
+            {
+                Console.WriteLine("L'avion décolle...\n");
+            }
+            public void Atterrir()
+            {
+                Console.WriteLine("L'avion atterrit...\n");
+            }
+        }
+        class Projet : IVolant
+        {
+            public void Decoller()
+            {
+                Console.WriteLine("Le projet décolle...\n");
+            }
+            public void Atterrir()
+            {
+                Console.WriteLine("Le projet atterit...\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<IVolant> volants = new List<IVolant>();
+            volants.Add(new Oiseau());
+            volants.Add(new Avion());
+            volants.Add(new Projet());
+            foreach (IVolant v in volants)
+            {
+                v.Decoller();
+                v.Atterrir();
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
+        
+    
